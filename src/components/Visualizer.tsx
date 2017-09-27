837e6c2d6b47cfc7979cd59d79c74869aa716796
S. M. Catala <stephane@zenyway.com>
2017-09-27 17:37:34 +0200

    add functional code

---
+++ src/components/Visualizer.tsx
@@ -0,0 +1,9 @@
+import createElement from 'inferno-create-element';
+
+/*
+ * This is example of Inferno functional component
+ * Functional components provide great performance but does not have state
+ */
+export function Visualizer({ number: number }) {
+	return <div className="visualizer">{number}</div>;
+}
